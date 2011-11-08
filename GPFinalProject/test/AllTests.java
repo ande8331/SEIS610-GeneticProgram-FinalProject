18f86ab441b74dc3412a13508f42532c0213e156
RossA <ande3171@gmail.com>
2011-11-08 12:11:36 -0600

    Start storing unit tests

---
+++ GPFinalProject/test/AllTests.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ GPNodeValueTest.class, GPNodeOperatorTest.class })
+public class AllTests {
+
+}
---
+++ GPFinalProject/test/GPNodeOperatorTest.java
@@ -0,0 +1,58 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class GPNodeOperatorTest {
+
+	@Test
+	public void testEvaluateFitnessValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetGPDepth() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetGPNodeCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetGPString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindNodeReferenceById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGPNodeOperator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLeftNode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRightNode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetLeftNode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetRightNode() {
+		fail("Not yet implemented");
+	}
+
+}
