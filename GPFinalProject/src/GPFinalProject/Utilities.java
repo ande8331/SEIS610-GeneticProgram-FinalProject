1d6bfd5390eb4f733d33e9d463f1d16d53830547
RossA <ande3171@gmail.com>
2011-11-08 21:41:08 -0600

    Issue when too many candidates are killed off and we try to generate a random number with a negative base.

--- GPFinalProject/src/GPFinalProject/Utilities.java
+++ GPFinalProject/src/GPFinalProject/Utilities.java
@@ -7,10 +7,19 @@ public class Utilities {
 	public static int GetRandomNumber(int minimumNum, int maximumNum)
 	{
 		int diff = maximumNum - minimumNum +1;
+		if (diff < 1)
+		{
+			return 0;
+		}
 		Random rand = new Random();
 		int output = rand.nextInt(diff);
 		output += minimumNum;		
 		return output;
+	}
+	
+	public static double GetRandomDouble()
+	{
+		return Math.random();
 	}
 	
 }
